137c9ffbde6a68a8ef6b8ded6fec995909fe43f8
igedanielolumide@gmail.com <igedanielolumide@gmail.com>
2022-08-23 22:34:45 +0100

    feat: game logic

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import GameScreen from './screen/GameScreen';
+import Header from './components/Header';
+import StartGameScreen from './screen/StartGameScreen';
+import { useState } from 'react';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0)
+
+  const gameOverHamdler = numOfRounds => {
+    setUserNumber(numOfRounds);
+    setGuessRounds(0)
+  }
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber)
+  };
+  let content = <StartGameScreen onStartGame={startGameHandler}/>;
+
+  if (userNumber){
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHamdler}/>; 
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Header title='Guess a Number'/>
+      {content}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
---
+++ screen/GameOver.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const GameOver = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>The game is over!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default GameOver;
